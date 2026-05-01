a19979bb19807bcaa887dc3c242ffb167d48c38f
nekoguntai <253817213+nekoguntai@users.noreply.github.com>
2026-05-01 08:19:44 -1000

    Split transaction export contract tests (#245)

--- server/tests/unit/api/transactions-http-routes.test.ts
+++ server/tests/unit/api/transactions-http-routes.test.ts
@@ -2,12 +2,14 @@ import { describe } from 'vitest';
 
 import { registerTransactionHttpBroadcastTests } from './transactionsHttpRoutes/transactionsHttpRoutes.broadcast.contracts';
 import { registerTransactionHttpCreationTests } from './transactionsHttpRoutes/transactionsHttpRoutes.creation.contracts';
+import { registerTransactionHttpExportTests } from './transactionsHttpRoutes/transactionsHttpRoutes.exports.contracts';
 import { registerTransactionHttpReadTests } from './transactionsHttpRoutes/transactionsHttpRoutes.reads.contracts';
 import { setupTransactionHttpRouteHooks } from './transactionsHttpRoutes/transactionsHttpRoutesTestHarness';
 
 describe('Transaction HTTP Routes', () => {
   setupTransactionHttpRouteHooks();
   registerTransactionHttpReadTests();
+  registerTransactionHttpExportTests();
   registerTransactionHttpCreationTests();
   registerTransactionHttpBroadcastTests();
 });
